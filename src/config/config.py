0e2046f40ada7b17e99e490f3d95d48e76ef50e4
williamk19 <williamkurniawan1144@gmail.com>
2023-10-11 16:50:07 +0700

    fix: postgresql configuration

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,11 @@
 from starlette.config import Config
 
 config = Config(".env")
-JWT_SECRET = config("JWT_SECRET", default='secret-key')
-JWT_ALGO = config("JWT_ALGO", default='HS256')
+JWT_SECRET = config("JWT_SECRET", default="secret-key")
+JWT_ALGO = config("JWT_ALGO", default="HS256")
 JWT_EXPIRE = config("JWT_EXPIRE", default=200000)
+
+DB_NAME = config("DB_NAME", default="todo_db")
+DB_USERNAME = config("DB_USERNAME", default="root")
+DB_PASSWORD = config("DB_PASSWORD", default="root")
+DB_HOST = config("DB_HOST", default="localhost")
--- src/database/database.py
+++ src/database/database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from ..config import config
+SQLALCHEMY_DB_URL = f"postgresql://{config.DB_USERNAME}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}"
 
-SQLALCHEMY_DB_URL = "sqlite:///./todos.db"
-
-engine = create_engine(SQLALCHEMY_DB_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
