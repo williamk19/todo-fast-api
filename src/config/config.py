45a80ef59dccb7e26b055cc4eda99a5aa8d895dc
williamk19 <williamkurniawan1144@gmail.com>
2023-10-12 14:57:20 +0700

    fix: config improvement db

--- src/config/config.py
+++ src/config/config.py
@@ -5,6 +5,8 @@ JWT_SECRET = config("JWT_SECRET", default="secret-key")
 JWT_ALGO = config("JWT_ALGO", default="HS256")
 JWT_EXPIRE = config("JWT_EXPIRE", default=200000)
 
+DB_MS = config("DB_MS", default="postgresql")
+DB_PORT = config("DB_PORT", default="5432")
 DB_NAME = config("DB_NAME", default="todo_db")
 DB_USERNAME = config("DB_USERNAME", default="root")
 DB_PASSWORD = config("DB_PASSWORD", default="root")
--- src/database/database.py
+++ src/database/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from ..config import config
-SQLALCHEMY_DB_URL = f"postgresql://{config.DB_USERNAME}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}"
+
+SQLALCHEMY_DB_URL = f"{config.DB_MS}://{config.DB_USERNAME}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
