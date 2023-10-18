45adff9e5d729dfa4f8a77746babea03b6907d12
williamk19 <williamkurniawan1144@gmail.com>
2023-10-18 09:23:41 +0700

    fix: change python version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from src.models import model
 from src.database.database import engine
 from src.routers import auth, todos, admin, users
@@ -16,3 +17,6 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
 app.include_router(users.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
